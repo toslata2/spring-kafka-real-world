3929ddb69ae5f2fb34b4f316746f237ba5228a29
kasin-ua <toslata@gmail.com>
2023-03-01 22:29:02 +0700

    init

---
+++ src/main/java/com/example/springkafkarealworldproject/SpringKafkaRealWorldProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.springkafkarealworldproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaRealWorldProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaRealWorldProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springkafkarealworldproject/SpringKafkaRealWorldProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springkafkarealworldproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaRealWorldProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
